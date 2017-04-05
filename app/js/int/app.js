665542a6e67aab9b2e9fa9e955ba8ed0f625e0b5
Sakke <sakkeh@gmail.com>
2017-04-05 23:27:03 +0300

    Basic POC

---
+++ app/js/int/app.js
@@ -0,0 +1,24 @@
+// Define the taxdata module
+var taxdata = angular.module('taxdata', []);
+
+// Define the `MainController` controller on the `taxdata` module
+taxdata.controller('MainController', function MainController($scope) {
+  $scope.queries = [
+    {
+      hakupvm: '1.3.2017',
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.',
+      received: '14.3.2017'
+    }, {
+      hakupvm: '1.3.2017',	
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.',
+      received: '12.3.2017'
+    }, {
+      hakupvm: '1.3.2017',
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.',
+      received: '11.3.2017',
+    }
+  ];
+});
